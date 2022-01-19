225819463c4a6a154e457c40b2a35e9cc24da006
yudistira1231 <yudistira.belajar@gmail.com>
2022-01-19 07:47:35 +0700

    pertemuan 15 belajar view engine expressjs ada error saat setup modulnya

---
+++ 15-express-view-engine/app.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+const { getMaxListeners } = require('process')
+const app = express()
+const port = 3000
+
+// gunakan ejs
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+    // res.sendFile('./index.html', { root: __dirname })
+    const mahasiswa = [
+    {
+        nama: 'Yudistira Yuda',
+        email: 'yudistira@gmail.com',
+    },
+    {
+        nama: 'Fathur Rohman',
+        email: 'fathur@gmail.com',
+    },
+    {
+        nama: 'Muhammad Faisal',
+        email: 'faisal@gmail.com',
+    },
+]
+    res.render('index', { 
+        nama: 'Yudistira Yuda', 
+        title: 'Halaman Home',
+        mahasiswa,
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', { 
+        layout: 'layouts/main-layout',
+        title: 'Halaman About'
+     });
+})
+
+app.get('/Contact', (req, res) => {
+    res.render('contact', { 
+        layout: 'layouts/main-layout',
+        title: 'Halaman Contact'
+     });
+})
+
+app.get('/product/:id', (req, res) => {
+    res.send(`Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`)
+})
+
+app.use('', (req, res) => {
+    res.status(404)
+    res.send('<h1>404</h1>')
+}) 
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
