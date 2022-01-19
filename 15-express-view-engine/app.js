e018399672397be613f01aa9259cc25badc5f1a8
yudistira1231 <yudistira.belajar@gmail.com>
2022-01-19 09:40:34 +0700

    error berhasil diselesaikan pertemuan 15 selesai

--- 15-express-view-engine/app.js
+++ 15-express-view-engine/app.js
@@ -1,11 +1,13 @@
 const express = require('express')
-
+const expressLayouts = require('express-ejs-layouts')
 const { getMaxListeners } = require('process')
 const app = express()
 const port = 3000
 
 // gunakan ejs
 app.set('view engine', 'ejs')
+app.use(expressLayouts)
+app.set("layouts extractScripts", true)
 
 app.get('/', (req, res) => {
     // res.sendFile('./index.html', { root: __dirname })
@@ -23,21 +25,24 @@ app.get('/', (req, res) => {
         email: 'faisal@gmail.com',
     },
 ]
-    res.render('index', { 
+    res.render('layout', { 
         nama: 'Yudistira Yuda', 
         title: 'Halaman Home',
         mahasiswa,
+        layout: 'layouts/main-layout'
     });
 })
 
 app.get('/about', (req, res) => {
     res.render('about', { 
+        layout: 'layouts/main-layout',
         title: 'Halaman About',
      });
 })
 
 app.get('/Contact', (req, res) => {
     res.render('contact', { 
+        layout: 'layouts/main-layout',
         title: 'Halaman Contact',
      });
 })
