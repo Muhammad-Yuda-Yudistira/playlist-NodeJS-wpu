11de7fc9d10aa25540d859a7fe59340158a64c78
yudistira1231 <yudistira.belajar@gmail.com>
2022-04-12 20:45:22 +0700

    video 9 menggunakan npm

--- praktek/8-npm/app.js
+++ praktek/8-npm/app.js
@@ -1,1 +1,14 @@
-console.log("Hello world!")
+const validator = require('validator')
+const chalk = require('chalk')
+
+// const email = validator.isEmail('yudistira@gmail.c')
+// console.log(validator.isMobilePhone('082123456789', 'id-ID'))
+// const nama = "51"
+// console.log(validator.isNumeric(nama))
+
+// console.log(email)
+const nama = 'yudistira'
+const umur = 21
+// console.log(chalk.bgGreen.underline.yellow('Hello world!'))
+// console.log(chalk.bgCyanBright.underline.inverse('Hello', 'world!'))
+console.log(chalk `Hello nama {yellow saya ${nama}} umur saya{bgRed ${umur} tahun}.`)
